4abd1440fd7bd37f21b3ea51ea1e960f85dd3889
devbong92 <web.bong92@gmail.com>
2024-09-16 13:36:10 +0900

    Docs: 주석 수정

--- src/routes/teams.router.js
+++ src/routes/teams.router.js
@@ -4,10 +4,9 @@ import TeamsController from '../controllers/teams.controller.js';
 const router = express.Router();
 const teamsController = new TeamsController();
 
-/**
- * 선발 등록 API
- */
+/** 선발 등록 API */
 router.post('/teams/starting', teamsController.addStarting);
+/** 선발 해제 API */
 router.delete('/teams/starting', teamsController.subtractStarting);
 
 export default router;
